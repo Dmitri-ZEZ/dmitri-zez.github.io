75e2fbe1b244dddbc977b80019c1159926c2f03f
Dmitri-ZEZ <zezyulya.d@gmail.com>
2021-06-21 20:06:46 +0800

    Add files via upload

---
+++ MAIN2/slider/custom.js
@@ -0,0 +1,12 @@
+jQuery(function($){
+
+    $(document).ready(function(){
+        $('.autoplay').slick({
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+          });
+    });
+
+});
